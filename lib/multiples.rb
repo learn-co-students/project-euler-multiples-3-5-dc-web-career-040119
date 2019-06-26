7170ff3e86a258daa309781af9eb4080983beada
github username <github email address>
2019-06-26 10:00:23 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,17 @@
 # Enter your procedural solution here!
+require('pry')
+def collect_multiples(limit)
+  i = 1
+  multiples_of_3_and_5 = []
+  while i < limit
+    if i % 3 == 0 || i % 5 == 0
+      multiples_of_3_and_5 << i
+    end
+    i += 1
+  end
+  multiples_of_3_and_5
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(0){| sum, n| sum + n }
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  attr_reader :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+
+  def collect_multiples
+    i = 1
+    multiples_of_3_and_5 = []
+    while i < self.limit
+      if i % 3 == 0 || i % 5 == 0
+        multiples_of_3_and_5 << i
+      end
+      i += 1
+    end
+    multiples_of_3_and_5
+  end
+
+  def sum_multiples
+    self.collect_multiples.inject(0){ |sum, n| sum + n }
+  end
+end
